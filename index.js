a2b4d0825e4a13083a94513569478ba55eaec0f6
hallysonmsilva-sketch <hallysonmsilva@gmail.com>
2026-05-20 17:06:52 -0300

    fix shuffle

--- index.js
+++ index.js
@@ -92,7 +92,16 @@ app.get('/generate-lote', async (req, res) => {
   // EMBARALHAR
   // =====================================
 
-  lista.sort(() => Math.random() - 0.5);
+  for (let i = lista.length - 1; i > 0; i--) {
+
+  const j = Math.floor(
+    Math.random() * (i + 1)
+  );
+
+  [lista[i], lista[j]] =
+    [lista[j], lista[i]];
+
+}
 
   // =====================================
   // INSERIR
